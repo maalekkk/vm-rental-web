8dfad879080eb4d1da881f225df5651f0aaabcad
Nawrok <nawrok999@gmail.com>
2020-12-10 13:48:25 +0100

    Added logger for LoginController

--- src/main/java/pl/vmrent/web/controller/auth/LoginController.java
+++ src/main/java/pl/vmrent/web/controller/auth/LoginController.java
@@ -1,17 +1,22 @@
 package pl.vmrent.web.controller.auth;
 
 import pl.vmrent.web.service.UserService;
+import pl.vmrent.web.util.MessageProvider;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Named
 @RequestScoped
 public class LoginController
 {
+    private static final Logger logger = Logger.getLogger(LoginController.class.getSimpleName());
+
     String username;
     String password;
 
@@ -21,6 +26,9 @@ public class LoginController
     @Inject
     private HttpServletRequest request;
 
+    @Inject
+    private MessageProvider messageProvider;
+
     public String submit()
     {
         if (userService.findUserByUsername(username).isPresent())
@@ -28,11 +36,12 @@ public class LoginController
             try
             {
                 request.login(username, password);
+                logger.log(Level.INFO, username + " " + messageProvider.getMessage("messages", "login_succeed"));
                 return "dashboard";
             }
             catch (ServletException ignored)
             {
-                //TODO LOGGER
+                logger.log(Level.WARNING, username + " " + messageProvider.getMessage("messages", "login_failed"));
             }
         }
         return "root";
