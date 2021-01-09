e1a23aac5a016bd7fc4834be346ce287dd735347
Nawrok <nawrok999@gmail.com>
2021-01-09 04:19:23 +0100

    Added another Client user in repo init

--- src/main/java/pl/vmrent/web/repository/UserRepository.java
+++ src/main/java/pl/vmrent/web/repository/UserRepository.java
@@ -1,14 +1,15 @@
 package pl.vmrent.web.repository;
 
-import pl.vmrent.web.model.user.Role;
 import pl.vmrent.web.model.user.User;
 import pl.vmrent.web.repository.generator.UuidGenerator;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.validation.Valid;
-import java.util.Collections;
 import java.util.UUID;
+
+import static java.util.Collections.singleton;
+import static pl.vmrent.web.model.user.Role.*;
 
 @ApplicationScoped
 public class UserRepository extends InMemoryRepository<@Valid User, UUID>
@@ -21,11 +22,13 @@ public class UserRepository extends InMemoryRepository<@Valid User, UUID>
     @PostConstruct
     private void init()
     {
-        User u1 = new User("Nawrok", "trudnehaslo", "Sebastian Nawrocki", true, Collections.singleton(Role.Owner));
-        User u2 = new User("Blazz", "trudnehaslo", "Maciej Błażewicz", true, Collections.singleton(Role.Admin));
-        User u3 = new User("Malek", "trudnehaslo", "Bartłomiej Małkowski", true, Collections.singleton(Role.Client));
+        User u1 = new User("Nawrok", "trudnehaslo", "Sebastian Nawrocki", true, singleton(Owner));
+        User u2 = new User("Blazz", "trudnehaslo2", "Maciej Błażewicz", true, singleton(Admin));
+        User u3 = new User("Malek", "trudnehaslo3", "Bartłomiej Małkowski", true, singleton(Client));
+        User u4 = new User("Rafon", "trudnehaslo4", "Marcin Krasucki", false, singleton(Client));
         save(u1);
         save(u2);
         save(u3);
+        save(u4);
     }
 }
