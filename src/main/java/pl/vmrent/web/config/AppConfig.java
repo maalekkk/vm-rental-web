c38330c53533867665119d81864a8a40c9c4697f
Nawrok <nawrok999@gmail.com>
2021-01-09 04:24:04 +0100

    Implemented JwtAuthenticationMechanism Disabled previous auth mechanism

--- src/main/java/pl/vmrent/web/config/AppConfig.java
+++ src/main/java/pl/vmrent/web/config/AppConfig.java
@@ -2,16 +2,14 @@ package pl.vmrent.web.config;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
-import javax.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
-import javax.security.enterprise.authentication.mechanism.http.LoginToContinue;
 
-@CustomFormAuthenticationMechanismDefinition(
+/*@CustomFormAuthenticationMechanismDefinition(
         loginToContinue = @LoginToContinue(
                 loginPage = "/auth/login.xhtml",
                 errorPage = "/auth/login.xhtml",
                 useForwardToLogin = false
         )
-)
+)*/
 @FacesConfig
 @ApplicationScoped
 public class AppConfig
