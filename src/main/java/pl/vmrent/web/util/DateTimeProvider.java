f66ad25d717f828945b9b50c70a3e4726896b3da
Nawrok <nawrok999@gmail.com>
2021-01-09 04:21:38 +0100

    Added JWT TokenProvider

--- src/main/java/pl/vmrent/web/util/DateTimeProvider.java
+++ src/main/java/pl/vmrent/web/util/DateTimeProvider.java
@@ -3,6 +3,8 @@ package pl.vmrent.web.util;
 import javax.enterprise.context.ApplicationScoped;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
 
 @ApplicationScoped
 public class DateTimeProvider implements Serializable
@@ -10,5 +12,10 @@ public class DateTimeProvider implements Serializable
     public LocalDateTime now()
     {
         return LocalDateTime.now();
+    }
+
+    public Date toDate(LocalDateTime localDateTime)
+    {
+        return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
     }
 }
